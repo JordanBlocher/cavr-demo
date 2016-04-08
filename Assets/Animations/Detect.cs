84648a60350a24ad3720fcaa498fefc208a405a8
Jordan F Blocher <jblocher@unr.edu>
2016-04-08 06:33:34 -0700

    Scripts

---
+++ Assets/Animations/Detect.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Detect : MonoBehaviour
+{
+
+    public List<GameObject> Children;
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.tag == "Enemy")
+            {
+                Children.Add(child.gameObject);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            foreach (GameObject child in Children)
+            {
+                child.GetComponent<Goblin>().InRange = true;
+            }
+        }
+    }
+}
