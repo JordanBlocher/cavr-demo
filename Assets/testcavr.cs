4df6fe4272cddb1a7117cb105439f855eaea6b0b
Chase Carthen <chasec2331@gmail.com>
2016-04-08 02:22:56 -0700

    Added everything

---
+++ Assets/testcavr.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testcavr : MonoBehaviour {
+	public CaVR ca;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.LogError(ca.InputManger.GetButtonValue("attack"));
+	}
+}
